df0b7e969bc15f518275c59c9690c00155809b65
Guilherme Del Rio <Guilherme Del Rio@DESKTOP-PR09JIP>
2023-03-20 09:08:04 -0300

    Adicionar arquivos de projeto.

---
+++ WiredBrainCoffee.DataProcessorTests/Data/ConsoleCoffeeCountStoreTests.cs
@@ -0,0 +1,22 @@
+﻿using WiredBrainCoffee.DataProcessor.Model;
+
+namespace WiredBrainCoffee.DataProcessor.Data;
+
+public class ConsoleCoffeeCountStoreTests
+{
+    [Fact]
+    public void ShouldWriteOutputToConsole()
+    {
+        // Arrange
+        var item = new CoffeeCountItem("Cappuccino", 5);
+        var stringWriter = new StringWriter();
+        var consoleCoffeCountStore = new ConsoleCoffeeCountStore(stringWriter);
+
+        // Act
+        consoleCoffeCountStore.Save(item);
+
+        // Assert
+        var result = stringWriter.ToString();
+        Assert.Equal($"{item.CoffeeType}:{item.Count}{Environment.NewLine}", result);
+    }
+}
